cde7c13cc7ed96796c7210c20b7359c2681b612a
Calebe Cabral <calebecabral.dev@gmail.com>
2023-08-19 11:58:26 -0300

    Initial commit

---
+++ 01-foo-bar-final/src/test/java/br/com/mxti/foo/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.mxti.foo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
